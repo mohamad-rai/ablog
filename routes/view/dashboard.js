b159db7739c78a2c37a68efaaccc02a8209917cb
mohamad-rai <0918.mohamad@gmail.com>
2021-03-26 18:20:25 +0430

    show avatar from database in dashboard

--- routes/view/dashboard.js
+++ routes/view/dashboard.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const {accessControl} = require('../../tools/generalTools');
 
 router.get('/', accessControl, (req, res) => {
-    res.render('dashboard/index', {title: "dashboard", data: req.session.user, user: req.session.user});
+    res.render('dashboard/index', {title: "dashboard", data: req.session.user});
 });
 
 module.exports = router;
