3ba3ee46fc7e3a6c91c8ed57818a78fd0748c1f8
mohamad-rai <0918.mohamad@gmail.com>
2021-03-27 16:56:58 +0430

    add article route

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const user = require('./api/userRoute');
-const file = require('./api/file');
+const file = require('./api/fileRoute');
+const article = require('./api/articleRoute');
 
 router.use('/user', user);
 router.use('/file', file);
+router.use('/article', article);
 
 module.exports = router;
