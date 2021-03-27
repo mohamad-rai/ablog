4af9e5bff30459d639cd2469cd1cf5aa08c634fc
mohamad-rai <0918.mohamad@gmail.com>
2021-03-27 16:46:41 +0430

    add default article image in config

--- config/const.js
+++ config/const.js
@@ -5,3 +5,4 @@ exports.SECRET_SESSION = "UhWt34^fjEe9UWfh$9JxH*r$qSYKyeT=t=58ypkx#uKAtUack-WsLW
 exports.DATABASE_NAME = "ablog";
 exports.DATABASE_ADDRESS = "localhost:27017";
 exports.DEFAULT_AVATAR = "user.png";
+exports.DEFAULT_ARTICLE_IMAGE = "article.png";
